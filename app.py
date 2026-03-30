3276481b0bf529c1e15f0703d6aaedf91b274696
gemps254-ai <gemps254@gmail.com>
2026-03-30 17:55:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -178,11 +178,19 @@ with tab2:
                 
                 with col_left:
                     st.write("**Sales Log**")
-                    st.dataframe(u_sales[["Date", "CounterpartyPIN", "Total", "VAT"]].tail(10), use_container_width=True, hide_index=True)
+                    st.dataframe(u_sales[["Date", "CounterpartyPIN", "Total", "VAT"]].tail(10), use_container_width=True, hide_index=True, column_config={
+        "Total": st.column_config.NumberColumn(format="KES %d"),
+        "VAT": st.column_config.NumberColumn(format="KES %d")
+    }
+))
                 
                 with col_right:
                     st.write("**Purchases Log**")
-                    st.dataframe(u_purch[["Date", "CounterpartyPIN", "Total", "VAT"]].tail(10), use_container_width=True, hide_index=True)
+                    st.dataframe(u_purch[["Date", "CounterpartyPIN", "Total", "VAT"]].tail(10), use_container_width=True, hide_index=True, column_config={
+        "Total": st.column_config.NumberColumn(format="KES %d"),
+        "VAT": st.column_config.NumberColumn(format="KES %d")
+    }
+))
                 
             except Exception as e:
                 st.error(f"Syntax or Connection Error: {e}")
